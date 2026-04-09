3b102e096b72edf86d6200caf5e0c837924d3ac1
DironWeit <arseneiantonov27@gmail.com>
2026-04-10 01:05:12 +0300

    Added a favorites button

---
+++ src/FavoriteButton.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export function FavoriteButton() {
+  const [isFavorite, setIsFaforite] = useState(false)
+  return (<button onClick={() => setIsFaforite(!isFavorite)}> 
+    {isFavorite ? '❤️' :'😍'}
+  </button>
+  )
+}
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,3 +1,5 @@
+import { FavoriteButton } from "./FavoriteButton";
+
 export function MovieCard({ image, rating }) {
   return (
     <div className="relative w-[200px] rounded-2xl overflow-hidden bg-neutral-900 shadow-lg hover:scale-105 transition-transform will-change-transform duration-300">
@@ -5,6 +7,11 @@ export function MovieCard({ image, rating }) {
         src={image}
         alt="Movie Poster"
         className="w-full h-auto object-cover" />
+      
+      {/* Кнопка избранного */}
+      <div className="absolute top-2 right-2 z-10">
+        <FavoriteButton/>
+      </div>
       <div className="absolute bottom-0 left-0 w-full bg-gragient-to-t from-black/80 to-transparent p-2 text-sm text-white font-semibold">
         IMDb: {rating}
       </div>
