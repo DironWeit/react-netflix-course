abb66c4a5eb169056f93dbf566e6e6e78688e49e
DironWeit <arseneiantonov27@gmail.com>
2026-04-10 00:38:09 +0300

    Editing styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { MovieCard } from "./MovieCard";
 
 function App() {
   return (
-    <>
-      <MovieCard image="/1920x.webp" rating={8.8} />
-    </>
-  );
+    <div className="min-h-screen w-full bg-black text-white px-6 py-5">
+      <header className="mb-10 flex items-center">
+        <img
+          src="/netflix.png"
+          alt="Netflix"
+          className="h-8 w-auto"
+        />
+      </header>
+      <main className="flex gap-6">
+        <MovieCard image="/narcos.jpg" rating={8.8} />
+      </main>
+    </div>
+  )
 }
-
-export default App;
+export default App
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,8 +1,13 @@
 export function MovieCard({ image, rating }) {
   return (
-    <div>
-      <div>IMDb rating: {rating}</div>
-      <img src={image} alt="" width={200}/>
+    <div className="relative w-[200px] rounded-2xl overflow-hidden bg-neutral-900 shadow-lg hover:scale-105 transition-transform will-change-transform duration-300">
+      <img
+        src={image}
+        alt="Movie Poster"
+        className="w-full h-auto object-cover" />
+      <div className="absolute bottom-0 left-0 w-full bg-gragient-to-t from-black/80 to-transparent p-2 text-sm text-white font-semibold">
+        IMDb: {rating}
+      </div>
     </div>
-  );
+  )
 }
