bcd4b7d24d6df8e91bf390d13e09f5c0766a6384
DironWeit <arseneiantonov27@gmail.com>
2026-04-10 18:44:55 +0300

    create useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { MovieCard } from "./MovieCard";
 import { MOVIES } from "./movies.data";
 
--- src/FavoriteButton.jsx
+++ src/FavoriteButton.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react"
+import {useEffect, useState } from "react"
 
 export function FavoriteButton() {
   const [isFavorite, setIsFaforite] = useState(false)
+
+  useEffect(() => {
+      console.log("Изменяем состояние isFavorite = ", isFavorite)
+    },[isFavorite])
+
   return (<button onClick={() => setIsFaforite(!isFavorite)}> 
     {isFavorite ? '❤️' :'🤍'}
   </button>
